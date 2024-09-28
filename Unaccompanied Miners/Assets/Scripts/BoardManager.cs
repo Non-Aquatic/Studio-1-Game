edf43aeb00dda0c6e052e9be60865c78a652af92
ChantingSplash <160074553+ChantingSplash@users.noreply.github.com>
2024-09-28 17:39:20 -0400

    Add Juan's changes

---
+++ Unaccompanied Miners/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public GameObject tileContainer; 
+    public GameObject traversableTilePrefab;
+    public GameObject nonTraversableTilePrefab;
+    public GameObject miningTilePrefab;
+
+    public int[,] gridLayout = {
+        { 1, 2, 0, 1, 1, 0, 1 },
+        { 1, 0, 0, 1, 0, 0, 1 },
+        { 1, 1, 1, 1, 1, 0, 1 },
+        { 0, 0, 0, 0, 1, 1, 1 },
+        { 1, 1, 1, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 1, 0, 1 },
+        { 1, 1, 1, 1, 1, 1, 1 }
+    };
+
+    private void Start()
+    {
+        GenerateBoard();
+    }
+    private void GenerateBoard()
+    {
+        int rows = gridLayout.GetLength(0);
+        int columns = gridLayout.GetLength(1);
+
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < columns; x++)
+            {
+                GameObject tilePrefab;
+                if (gridLayout[y, x] == 1)
+                {
+                    tilePrefab = traversableTilePrefab;
+                }
+                else if (gridLayout[y, x] == 2)
+                {
+                    tilePrefab = miningTilePrefab; 
+                }
+                else
+                {
+                    tilePrefab = nonTraversableTilePrefab;
+                }
+                GameObject tileInstance = Instantiate(tilePrefab, new Vector3(x, 0, y), Quaternion.identity);
+                tileInstance.transform.SetParent(tileContainer.transform);
+            }
+        }
+    }
+
+    public bool IsTileTraversable(Vector2Int position)
+    {
+        if (position.x < 0 || position.x >= gridLayout.GetLength(1) || position.y < 0 || position.y >= gridLayout.GetLength(0))
+            return false;
+        return gridLayout[position.y, position.x] == 1 || gridLayout[position.y, position.x] == 2;
+    }
+    public bool IsMiningNode(Vector2Int position)
+    {
+        if (position.x < 0 || position.x >= gridLayout.GetLength(1) || position.y < 0 || position.y >= gridLayout.GetLength(0))
+            return false;
+
+        return gridLayout[position.y, position.x] == 2; 
+    }
+}
